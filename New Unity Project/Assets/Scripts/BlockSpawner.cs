4a3eabf113c359db6cd9206fa94498a2c2c38995
mfakir <46379342+mfakirr@users.noreply.github.com>
2021-03-04 14:35:55 +0300

    first commit
    Create sprite with cubes and charaacter movement is done.

---
+++ New Unity Project/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Cube = default;
+
+    Vector3 blockPos = Vector3.zero;
+    [SerializeField]
+    Texture2D mySprite = default;
+    [SerializeField]
+    float spriteSize = 0;
+
+    private void OnEnable()
+    {
+        for (int x = 0; x < mySprite.height; x++)
+        {
+            for (int y = 0; y < mySprite.width; y++)
+            {
+                Color color = mySprite.GetPixel(x, y);
+                print("b");
+                if (color.a == 0)
+                {
+                    continue;
+                }
+
+                blockPos = new Vector3(
+                    spriteSize * (x - (mySprite.width * .5f)),
+                    spriteSize * .5f,
+                    spriteSize * (y - (mySprite.height * .5f)));
+
+                GameObject cubeObj = Instantiate(Cube, transform);
+                cubeObj.transform.localPosition = blockPos;
+
+                cubeObj.GetComponent<Renderer>().material.color = color;
+                cubeObj.transform.localScale = Vector3.one * spriteSize;
+            }
+
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+    Vector3 Startposition;
+    Vector3 direction;
+
+    Vector2 beforeMouseChange = Vector2.zero;
+    Vector2 afterMouseChange = Vector2.zero;
+
+    Rigidbody rb = default;
+
+    bool IsWalkable = false;
+    [SerializeField]
+    float speed = 0;
+    Vector3 stop = Vector3.zero;
+
+    private void Start()
+    {
+        rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+{
+    if (Input.GetMouseButtonDown(0))
+    {
+        Startposition = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y);
+    }
+
+    beforeMouseChange = Input.mousePosition;
+
+    if (Input.GetMouseButton(0) )
+    {
+        direction = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y) - Startposition;
+        IsWalkable = true;
+        }
+        else
+        {
+            direction = Vector3.zero;
+        }
+
+    afterMouseChange = Input.mousePosition;
+
+    }
+
+    private void FixedUpdate()
+    {
+    Move();
+
+    Rotate();
+    }
+
+    private void Rotate()
+    {
+
+        transform.LookAt((transform.position + direction));
+
+    }
+
+private void Move()
+{
+    if (IsWalkable )
+    {
+            if (direction.magnitude > 1)
+            {
+                rb.velocity = -direction.normalized * speed * Time.fixedDeltaTime;
+            }
+        }
+    }
+
+    bool CheckIsMouseMoving()
+    {
+          if (Vector2Int.RoundToInt(beforeMouseChange) != Vector2Int.RoundToInt(afterMouseChange))
+          {
+              return true;
+           
+        }
+          else
+          {
+              return false;
+          
+        }
+       
+    }
+}
